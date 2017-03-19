1851e9af104a09844120103286611b0aa5c225e0
knkanna8 <knkanna87@gmail.com>
2017-03-19 15:21:21 -0230

    Add files via upload

---
+++ R/data.R
@@ -0,0 +1,43 @@
+#' Fatal injuries suffered in motor vehicle traffic crashes in 2013
+#'
+#' It is a data from the US National Highway Traffic Safety Administration's
+#' Fatality Analysis Reporting System, which is a nationwide census providing
+#' the American public yearly data regarding fatal injuries suffered in motor
+#' vehicle traffic crashes.
+#'
+#' @format A data frame with columns:
+#' @examples
+#' \dontrun{
+#'  accident_2013
+#' }
+"accident_2013"
+
+#' Fatal injuries suffered in motor vehicle traffic crashes in 2014
+#'
+#' It is a data from the US National Highway Traffic Safety Administration's
+#' Fatality Analysis Reporting System, which is a nationwide census providing
+#' the American public yearly data regarding fatal injuries suffered in motor
+#' vehicle traffic crashes.
+#'
+#' @format A data frame with columns:
+#' @examples
+#' \dontrun{
+#'  accident_2014
+#' }
+"accident_2014"
+
+#' Fatal injuries suffered in motor vehicle traffic crashes in 2015
+#'
+#' It is a data from the US National Highway Traffic Safety Administration's
+#' Fatality Analysis Reporting System, which is a nationwide census providing
+#' the American public yearly data regarding fatal injuries suffered in motor
+#' vehicle traffic crashes.
+#'
+#' @format A data frame with columns:
+#' @examples
+#' \dontrun{
+#'  accident_2015
+#' }
+"accident_2015"
+
+
